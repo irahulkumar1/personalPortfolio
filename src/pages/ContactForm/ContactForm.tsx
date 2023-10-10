c82c79aabe4692d304a3d63a8258c1f6957ed70d
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2023-10-11 00:23:12 +0530

    removed console

--- src/pages/ContactForm/ContactForm.tsx
+++ src/pages/ContactForm/ContactForm.tsx
@@ -10,9 +10,36 @@ export default function ContactForm() {
 
     const [formData, setFormData] = useState(initialFormData);
     const [successMessage, setSuccessMessage] = useState<string | null>(null);
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        phoneNumber: ''
+    });
+
+    const validateEmail = (email: string) => {
+        // Simple email validation regex
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return emailRegex.test(email);
+    };
+
+    const validatePhoneNumber = (phoneNumber: string) => {
+        // Simple phone number validation regex
+        const phoneRegex = /^\+?[0-9]*$/;
+        return phoneRegex.test(phoneNumber);
+    };
+
+
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        // Check if there are any errors before submitting
+        if (Object.values(errors).some((error) => error !== "")) {
+            // If there are errors, don't submit the form
+            console.error('Form submission failed due to validation errors');
+            return;
+        }
+
         try {
             const response = await fetch('https://script.google.com/macros/s/AKfycbyNHrWR7jvMIand6YUa0kzCbR8yjoJ_h_n4pwv3hQGtf1bc9rPTkiL3VcCpL-4c5e-PiA/exec', {
                 method: 'POST',
@@ -23,8 +50,6 @@ export default function ContactForm() {
             });
 
             if (response.ok) {
-                // Form submitted successfully
-                console.log('Form submitted successfully');
                 setSuccessMessage("Form submitted successfully!");
                 setFormData(initialFormData); // Clear form fields
             } else {
@@ -39,6 +64,16 @@ export default function ContactForm() {
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
+        // Validate input and update errors
+        if (name === "name" && value.trim() === "") {
+            setErrors({ ...errors, [name]: "Name is required" });
+        } else if (name === "email" && !validateEmail(value)) {
+            setErrors({ ...errors, [name]: "Invalid email address" });
+        } else if (name === "phoneNumber" && !validatePhoneNumber(value)) {
+            setErrors({ ...errors, [name]: "Invalid phone number" });
+        } else {
+            setErrors({ ...errors, [name]: "" }); // Clear error message
+        }
     };
 
     return (<>
@@ -56,6 +91,7 @@ export default function ContactForm() {
                     value={formData.name}
                     onChange={handleInputChange}
                 />
+                {errors.name && <p className="text-red-500">{errors.name}</p>}
             </div>
             <div className="mb-4">
                 <label htmlFor="phoneNumber" className="block text-gray-700 text-sm font-bold mb-2">
@@ -70,6 +106,7 @@ export default function ContactForm() {
                     value={formData.phoneNumber}
                     onChange={handleInputChange}
                 />
+                {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
             </div>
             <div className="mb-4">
                 <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
@@ -84,6 +121,7 @@ export default function ContactForm() {
                     value={formData.email}
                     onChange={handleInputChange}
                 />
+                {errors.email && <p className="text-red-500">{errors.email}</p>}
             </div>
             <div className="mb-4">
                 <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">
