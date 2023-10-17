69fd82016416147bca665eb301c5f319acccddce
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2023-10-18 02:12:37 +0530

    updated code and services section

--- src/pages/Works/Services.tsx
+++ src/pages/Works/Services.tsx
@@ -1,27 +1,4 @@
-const projects = [
-  {
-    name: 'Project 1 - Awesome React App',
-    description: "Dive into the magic world of React with this amazing project. We've crafted a stunning user interface that will leave you speechless.",
-    image: 'project1.jpg', // Add an image URL here
-    link: 'https://project1.com', // Add a project URL here
-    tags: ['React', 'Node.js', 'UI/UX'],
-  },
-  {
-    name: 'Project 2 - Web Design Wonderland',
-    description: 'Get lost in the mesmerizing world of web design. Explore a modern and responsive website created with HTML, CSS, and JavaScript.',
-    image: 'project2.jpg', // Add an image URL here
-    link: 'https://project2.com', // Add a project URL here
-    tags: ['HTML', 'CSS', 'JavaScript'],
-  },
-  {
-    name: 'Project 3 - Django Dreams',
-    description: 'Embark on a journey through the Django framework. This project showcases the power of Python, Django, and database management.',
-    image: 'project3.jpg', // Add an image URL here
-    link: 'https://project3.com', // Add a project URL here
-    tags: ['Python', 'Django', 'Database'],
-  },
-];
-
+import { projects } from "./workData"
 const Services = () => {
   return (
     <div className="container mx-auto pt-10">
