2dc3288ce035962afe12922c43b3a120af40fa6c
Rahul Kumar <irahulkv@gmail.com>
2024-01-04 08:47:51 +0000

    added motion

--- src/pages/Works/WorkPopUp.tsx
+++ src/pages/Works/WorkPopUp.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 import React, { useEffect, useRef } from 'react';
+import { motion } from "framer-motion";
 
 interface Project {
   name: string;
@@ -19,7 +20,14 @@ interface WorkPopUpProps {
 
 const WorkPopUp: React.FC<WorkPopUpProps> = ({ selectedProject, closeModal }) => {
   const modalRef = useRef<HTMLDivElement>(null);
-
+  const modalVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
+  };
   const handleOutsideClick = (e: MouseEvent) => {
     if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
       closeModal();
@@ -39,12 +47,25 @@ const WorkPopUp: React.FC<WorkPopUpProps> = ({ selectedProject, closeModal }) =>
   }
 
   return (
-    <div className="fixed inset-0 z-30 bg-black bg-opacity-50 flex justify-center items-center">
-      <div ref={modalRef} className="bg-white p-6 rounded-lg w-full max-w-screen-md relative transform transition-transform duration-300 ease-in-out overflow-y-auto">
+    <motion.div
+      className="fixed inset-0 z-30 bg-black bg-opacity-50 flex justify-center items-center"
+      initial="hidden"
+      animate="visible"
+      variants={modalVariants}
+      onClick={closeModal}
+    >
+      <motion.div
+        ref={modalRef}
+        className="bg-white p-6 rounded-lg w-full max-w-screen-md relative transform transition-transform duration-300 ease-in-out overflow-y-auto"
+        onClick={(e) => e.stopPropagation()}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+      >
         <button className="absolute top-4 right-4 text-gray-500" onClick={closeModal}>
           <AiOutlineCloseCircle size={24} />
         </button>
-        <h2 className="text-3xl font-bold mb-4">{selectedProject.name}</h2>
+        <h2 className="text-3xl font-bold mb-4">{selectedProject?.name}</h2>
         <div className="mb-4">
           <h3 className="text-xl font-semibold mb-2">Description</h3>
           <p className="text-gray-600">{selectedProject.description}</p>
@@ -72,8 +93,8 @@ const WorkPopUp: React.FC<WorkPopUpProps> = ({ selectedProject, closeModal }) =>
           )}
           <p className="text-gray-600">{selectedProject.organisationWorkedWith}</p>
         </div>
-      </div>
-    </div>
+        </motion.div>
+    </motion.div>
   );
 };
 
