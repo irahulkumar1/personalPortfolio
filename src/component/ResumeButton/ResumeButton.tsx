96d800131a63111748357e554383ededff1b89bb
Rahul Kumar <irahulkv@gmail.com>
2024-01-04 09:38:43 +0000

    resume download fix

--- src/component/ResumeButton/ResumeButton.tsx
+++ src/component/ResumeButton/ResumeButton.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import resumePDF from '/src/assets/Resume/Rahul_cv.pdf'; 
 
 export const Resume = () => {
     const [showProgressBar, setShowProgressBar] = useState(false);
+    const progressBarValue = 50; 
 
     const handleDownloadClick = () => {
         setShowProgressBar(true);
@@ -16,10 +18,13 @@ export const Resume = () => {
 
         downloadFile()
             .then(() => {
+
                 const link = document.createElement('a');
-                link.href = '/src/assets/Resume/Rahul_cv.pdf';
+                link.href = resumePDF; 
                 link.download = 'Rahul_cv.pdf';
+                document.body.appendChild(link);
                 link.click();
+                document.body.removeChild(link);
             })
             .catch((error) => {
                 console.error('Download error:', error);
@@ -32,8 +37,9 @@ export const Resume = () => {
     return (
         <>
             <button
-                className={`relative bg-highLighter hover:bg-[#ff577f] mt-3 text-white font-bold py-2 px-4 rounded-sm shadow-lg ${showProgressBar ? 'hidden' : ''
-                    }`}
+                className={`relative bg-highLighter hover:bg-[#ff577f] mt-3 text-white font-bold py-2 px-4 rounded-sm shadow-lg ${
+                    showProgressBar ? 'hidden' : ''
+                }`}
                 onClick={handleDownloadClick}
                 disabled={showProgressBar}
             >
@@ -43,7 +49,7 @@ export const Resume = () => {
                 <div className="relative w-[7rem] h-2 bg-[#ff577f] mt-3 rounded-full">
                     <div
                         className="absolute top-0 left-0 h-full bg-highLighter"
-                        style={{ width: `${showProgressBar}%` }}
+                        style={{ width: `${progressBarValue}%` }} // Use progressBarValue here
                     />
                 </div>
             )}
