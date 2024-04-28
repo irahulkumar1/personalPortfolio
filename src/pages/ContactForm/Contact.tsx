a805aa03fb2870727beb5003d35ad5fa0cb6c346
Rahul Kumar <irahulkv@gmail.com>
2024-04-29 00:20:08 +0530

    added new work in the portfolio (#57)
    Co-authored-by: Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>

--- src/pages/ContactForm/Contact.tsx
+++ src/pages/ContactForm/Contact.tsx
@@ -5,16 +5,19 @@ import SocialMedia from './SocailMedia';
 export default function Contact() {
   return (
     <>
-      {/* Laptop Screen */}
+      {/* Desktop Screen */}
       <section className="hidden lg:flex items-center h-screen">
         <section className="w-1/2 p-8 pr-24 flex justify-end items-center">
-          <div className="flex flex-col items-center">
-            <p className="text-lg">
-              Let’s make something new, different, and more meaningful or think more visual or conceptual?
-              <br/>
-              <span className='text-2xl mt-2'>Just say hello!</span>
+          <div className="flex flex-col items-start max-w-md">
+            <h2 className="text-4xl font-bold text-gray-800 mb-4 ">
+              Let’s make something new, different, and more meaningful.
+            </h2>
+            <p className="text-gray-600 mb-6 text-2xl font-bold font-caviateFont">
+              Whether you have a project in mind, a question, or just want to say hello, I’d love to hear from you.
             </p>
-            <span className='text-4xl mt-4'><BsArrowRight /></span>
+            <button className="flex items-center bg-highLighter hover:bg-[#ff577f] text-white font-bold py-2 px-4 rounded-full shadow-lgtransition-colors">
+              Say Hello <BsArrowRight className="ml-2" />
+            </button>
           </div>
         </section>
         <section className="w-1/2">
@@ -25,19 +28,23 @@ export default function Contact() {
       {/* Mobile Screen */}
       <main className="flex flex-col items-center justify-center lg:hidden">
         <section className="w-full p-8 text-center">
-          <div className="flex flex-col items-center">
-            <p className="text-lg">
-              Let’s make something new, different, and more meaningful or think more visual or conceptual?
-              <span className='text-2xl mt-2'>Just say hello!</span>
+          <div className="flex flex-col items-start max-w-md">
+            <h2 className="text-3xl font-bold text-gray-800 mb-4">
+              Let’s make something new, different, and more meaningful.
+            </h2>
+            <p className="text-lg text-gray-600 mb-6 font-caviateFont">
+              Whether you have a project in mind, a question, or just want to say hello, I’d love to hear from you.
             </p>
-            <span className='text-4xl mt-4'><BsArrowRight /></span>
+            <button className="flex items-center bg-highLighter hover:bg-[#ff577f] text-white py-2 px-4 rounded-full shadow-lg transition-colors">
+              Say Hello <BsArrowRight className="ml-2" />
+            </button>
           </div>
         </section>
         <section className="w-full">
           <ContactForm />
         </section>
       </main>
-      <section className="socialMedia bg-[#F0F0F0] flex-none w-full h-[13rem] lg:h-[11rem]">
+      <section className="socialMedia bg-gray-100 flex-none w-full h-[12rem] lg:h-[11rem]">
         <SocialMedia />
       </section>
     </>
